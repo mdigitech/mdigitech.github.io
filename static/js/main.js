f6f11f570baf3e5b910b2f48494ccde1d5bc673b
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-04-01 01:05:54 +0100

    added background

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold:1.0
+  };
+  let callback = (entries, observer)=>{
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.play();
+      }
+      else{
+        entry.target.pause();
+      }
+    });
+  }
+  let observer = new IntersectionObserver(callback, options);
+  observer.observe(document.querySelector('#video-bg'));
